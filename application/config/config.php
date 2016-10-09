9235256854ee8c350a298244855399d2eb4db4c3
Agustin Castiarena <castiarena@gmail.com>
2016-10-09 14:40:55 -0300

    change root config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/piedra-libre';
+$config['base_url'] = (ENVIRONMENT === 'production' ) ? 'http://www.fundacionpiedralibre.org' :'http://localhost/piedra-libre';
 
 /*
 |--------------------------------------------------------------------------
