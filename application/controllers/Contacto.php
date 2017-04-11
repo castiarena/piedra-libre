e1370b886d780bbd963cd6bb69dd0c30c92e4249
Agustin Castiarena <castiarena@gmail.com>
2017-04-11 01:25:04 -0300

    Envia mails

--- application/controllers/Contacto.php
+++ application/controllers/Contacto.php
@@ -11,13 +11,38 @@ class Contacto extends BaseController{
     public function __construct()
     {
         parent::__construct();
+        $this->load->library('email');
+        $config['mailtype'] = 'html';
+
+        $this->email->initialize($config);
+
     }
     public function index(){
-        $content = $this->load->view('contacto', [] ,true);
+        $data = ["status" => "no enviado"];
+        if($this->input->post('name')){
+            $data = $this->send();
+        }
+        $content = $this->load->view('contacto', $data ,true);
         $this->_render($content, 'Contacto');
     }
 
-    public function send(){
+    private function send(){
 
+        $this->email->from('webmaster@fundacionpiedralibre.org', $this->input->post('name') . ' '. $this->input->post('lastname'));
+        $this->email->to('fundacionarenales@gmail.com');
+
+        $this->email->subject('Contacto desde fundacionpiedralibre.org - ' . $this->input->post('subject'));
+
+        $message = "<strong>Escribio: </strong><br>". $this->input->post('name') . ' '. $this->input->post('lastname');
+        $message.= "<br><br>";
+        $message.= "<strong>Email: </strong><br> <a href='mailto:".$this->input->post('email')."'>".$this->input->post('email')."</a>";
+        $message.= "<br><br>";
+        $message.= "<strong>Asunto: </strong><br>". $this->input->post('subject');
+        $message.= "<br><br>";
+        $message.= "<strong>Comentarios: </strong><br>". $this->input->post('comments');
+        $this->email->message($message);
+
+        $this->email->send();
+        return ["status" => "enviado"];
     }
 }
--- application/views/contacto.php
+++ application/views/contacto.php
@@ -10,3 +10,79 @@
         <a href="#" class="color-blanco font-xl banner-scroll"></a>
     </div>
 </section>
+
+<section>
+    <div class="container container-l">
+        <form action="contacto" method="post">
+            <div class="row">
+                <div class="col-sm-12">
+                    <h2>Formulario de contacto</h2>
+                    <p>Consulta sobre como podes ayudarnos:</p>
+                    <?php if($status === 'enviado'):?>
+                    <p class="alert alert-success">Enviamos tu email, gracias por escribirnos</p>
+                    <?php endif;?>
+                </div>
+
+                <div class="col-sm-6">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label for="name">Nombre:</label>
+                                <input required class="form-control" type="text" name="name" id="name" placeholder="Nombre">
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <label for="name">Apellido:</label>
+                                <input required class="form-control" type="text" name="lastname" id="lastname" placeholder="Apellido">
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                    <div class="form-group">
+                        <label for="name">Email:</label>
+                        <input required class="form-control" type="text" name="email" id="email" placeholder="Email">
+                    </div>
+                    <div class="form-group">
+                        <label for="name">Asunto:</label>
+                        <input required class="form-control" type="text" name="subject" id="subject" placeholder="Asunto">
+                    </div>
+                    <div class="form-group">
+                        <label for="description">Comentarios:</label>
+                        <textarea required class="form-control" name="comments" id="comments" cols="30" rows="10"></textarea>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <p>El refugio de arenales:</p>
+                    <div class="img-thumbnail" id="map" style="width: 100%; height: 420px"></div>
+                </div>
+                <div class="col-sm-12">
+                    <button type="submit" class="btn btn-ghost-two">Enviar</button>
+                    <button type="reset" class="btn btn btn-ghost-two">Borrar</button>
+                </div>
+
+            </div>
+        </form>
+
+    </div>
+</section>
+
+<script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA1boInb_GxMo9JBBhlmIwr8Woa9I2Wk6E&callback=initMap">
+</script>
+<script>
+    function initMap() {
+        var arenales = {lat: -33.6251905, lng: -69.5178189};
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 12,
+            center: arenales,
+            mapTypeId: 'satellite'
+        });
+        var marker = new google.maps.Marker({
+            position: arenales,
+            map: map
+        });
+    }
+</script>
