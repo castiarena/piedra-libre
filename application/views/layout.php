a0429f0270a4583f33fecffee7ed7380cb9c757e
Agustin Castiarena <castiarena@gmail.com>
2017-07-10 00:43:05 -0300

    favicon

--- application/views/layout.php
+++ application/views/layout.php
@@ -13,6 +13,24 @@
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
 
+    <link rel="apple-touch-icon" sizes="57x57" href="<?= site_url('/apple-icon-57x57.png')?>">
+    <link rel="apple-touch-icon" sizes="60x60" href="<?= site_url('/apple-icon-60x60.png')?>">
+    <link rel="apple-touch-icon" sizes="72x72" href="<?= site_url('/apple-icon-72x72.png')?>">
+    <link rel="apple-touch-icon" sizes="76x76" href="<?= site_url('/apple-icon-76x76.png')?>">
+    <link rel="apple-touch-icon" sizes="114x114" href="<?= site_url('/apple-icon-114x114.png')?>">
+    <link rel="apple-touch-icon" sizes="120x120" href="<?= site_url('/apple-icon-120x120.png')?>">
+    <link rel="apple-touch-icon" sizes="144x144" href="<?= site_url('/apple-icon-144x144.png')?>">
+    <link rel="apple-touch-icon" sizes="152x152" href="<?= site_url('/apple-icon-152x152.png')?>">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= site_url('/apple-icon-180x180.png')?>">
+    <link rel="icon" type="image/png" sizes="192x192"  href="<?= site_url('/android-icon-192x192.png')?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= site_url('/favicon-32x32.png')?>">
+    <link rel="icon" type="image/png" sizes="96x96" href="<?= site_url('/favicon-96x96.png')?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= site_url('/favicon-16x16.png')?>">
+    <link rel="manifest" href="<?= site_url('/manifest.json')?>">
+    <meta name="msapplication-TileColor" content="#aaaaaa">
+    <meta name="msapplication-TileImage" content="<?= site_url('/ms-icon-144x144.png')?>">
+    <meta name="theme-color" content="#aaaaaa">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.7.1/css/bootstrap-datepicker.standalone.min.css">
     <link rel="stylesheet" href="<?= site_url('assets/css/styles.css')?>">
     <link rel="stylesheet" href="<?= site_url('assets/css/font-awesome.css')?>">
