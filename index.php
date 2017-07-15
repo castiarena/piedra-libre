9170668fcdaebb3186ae4b32448a0c03ca819486
Agustin Castiarena <castiarena@gmail.com>
2017-07-15 17:31:11 -0300

    enabling cors

--- index.php
+++ index.php
@@ -36,8 +36,6 @@
  * @filesource
  */
 
-header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Methods: GET, OPTIONS, POST, PUT, PATCH, DELETE");
 /*
  *---------------------------------------------------------------
  * APPLICATION ENVIRONMENT
